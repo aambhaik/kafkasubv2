73cbe925e48eb4ec6cec0b64b54ee5c734a3c695
Anant Ambhaikar <aambhaik@tibco.com>
2017-07-10 15:05:37 -0700

    use mashling library conditions

--- trigger.go
+++ trigger.go
@@ -21,8 +21,8 @@ import (
 	"github.com/TIBCOSoftware/flogo-lib/core/action"
 	"github.com/TIBCOSoftware/flogo-lib/core/trigger"
 	"github.com/TIBCOSoftware/flogo-lib/logger"
-	"github.com/aambhaik/resources/conditions"
-	"github.com/aambhaik/resources/util"
+	"github.com/TIBCOSoftware/mashling-lib/conditions"
+	"github.com/TIBCOSoftware/mashling-lib/util"
 )
 
 // log is the default package logger
